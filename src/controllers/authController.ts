14d764ce192e6347fb1e3015c3288bcb32618e9e
fatmaboodai <Fatmaboodai@hotmail.com>
2024-04-17 23:38:03 +0300

    2md draft

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,55 @@
 import { Request, Response } from "express";
+import User from "../models/User";
+import { generateToken, clearToken } from "../utils/auth";
 // Would handle user registration (e.g., saving user data to a database).
-const registerUser = (req: Request, res: Response) => {};
-// Would handle user authentication (e.g., verifying credentials).
-const authenticateUser = (req: Request, res: Response) => {};
-// Would handle user logout (e.g., clearing session data).
-const logoutUser = (req: Request, res: Response) => {};
+const registerUser = async (req: Request, res: Response) => {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+  
+    if (userExists) {
+      res.status(400).json({ message: "The user already exists" });
+    }
+  
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+  
+    if (user) {
+      generateToken(res, user._id);
+      res.status(201).json({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      });
+    } else {
+      res.status(400).json({ message: "An error occurred in creating the user" });
+    }
+  };
 
+
+// Would handle user authentication (e.g., verifying credentials).
+const authenticateUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.comparePassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401).json({ message: "User not found / password incorrect" });
+  }
+};
+
+// Would handle user logout (e.g., clearing session data).
+const logoutUser = (req: Request, res: Response) => {
+    clearToken(res);
+    res.status(200).json({ message: "User logged out" });
+  };
+  
 export { registerUser, authenticateUser, logoutUser };
